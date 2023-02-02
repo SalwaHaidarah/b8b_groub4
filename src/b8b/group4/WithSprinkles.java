15ba1ebd228e965e5a9e45ca3e6be58c236eef09
Samar_Alzaharany <samar.sweidd@gmail.com>
2023-02-02 18:28:24 +0300

    Commit

---
+++ src/b8b/group4/WithSprinkles.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b8b.group4;
+
+public class WithSprinkles extends CookiesDecorator{
+    
+    public WithSprinkles(Cookies cookies) {
+        super(cookies);
+    }
+   
+    
+    @Override
+    public double price(){
+        return super.price()+1;
+    }
+    
+    @Override
+    public String name(){
+        return super.name()+" with sprinkles";
+    }
+    
+    
+}
